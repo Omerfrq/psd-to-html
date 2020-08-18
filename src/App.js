56edea36fcdd5ea862b20040133db35c1552e245
Naeemkhan33 <ranker333@gmail.com>
2020-08-18 19:05:02 +0500

    added Footer

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import Data from './pages/data/data';
 import Navigation from './components/navigation/navigation';
 import Home from './pages/home/home';
 import Header from './components/header/header';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='vh-100 d-flex flex-column'>
       <Header />
       <Navigation />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/data' component={Data} />
       </Switch>
+      <Footer />
     </div>
   );
 }
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import './footer.styles.css;';
+import './footer.styles.css';
+import { Link } from 'react-router-dom';
 
-const Footer = () => { 
-    return ();
-}
+const Footer = () => {
+  return (
+    <footer className='Footer py-4 px-5 mt-auto'>
+      <Link>Terms & Conditions</Link>
+      <Link>All services | Policy</Link>
+    </footer>
+  );
+};
 
 export default Footer;
