744c1868bf3a2bd62c3768ac9fd91f470a055899
Naeemkhan33 <ranker333@gmail.com>
2020-08-19 21:41:23 +0500

    added manage-page

--- src/components/manage-existing/manage-existing.jsx
+++ src/components/manage-existing/manage-existing.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './manage-existing.styles.css';
-import { Button, Modal, Form, Table, Col } from 'react-bootstrap';
+import { Button, Modal, Form, Table, Col, Pagination } from 'react-bootstrap';
+import TableRow from './table-row';
 
 const tabletitle = [
   'Row',
@@ -22,68 +23,144 @@ const ManageExisting = ({ show, onHide, onSubmit }) => {
           <Table bordered hover responsive>
             <thead className='bg-light text-black-50 text-uppercase small'>
               <tr>
-                <th>Row</th>
-                <th>records count</th>
-                <th>saved name</th>
-                <th>created date time</th>
-                <th>last updated date time</th>
-                <th>actions</th>
+                <th className='position-relative'>
+                  <span>row</span>
+                  <span className='pos-top d-flex flex-column'>
+                    <Button
+                      variant='warning'
+                      size='sm'
+                      className='align-top p-0'
+                    >
+                      <h6 className='mb-0'>&times;</h6>
+                    </Button>
+                    <Button
+                      variant='warning'
+                      size='sm'
+                      className='align-top p-0'
+                    >
+                      <small className='mb-0'>▼</small>
+                    </Button>
+                  </span>
+                </th>
+                <th className='position-relative'>
+                  <span>records count</span>
+                  <span className='pos-top d-flex flex-column'>
+                    <Button
+                      variant='warning'
+                      size='sm'
+                      className='align-top p-0'
+                    >
+                      <h6 className='mb-0'>&times;</h6>
+                    </Button>
+                    <Button
+                      variant='warning'
+                      size='sm'
+                      className='align-top p-0'
+                    >
+                      <small className='mb-0'>▼</small>
+                    </Button>
+                  </span>
+                </th>
+                <th className='position-relative'>
+                  <span>saved name</span>
+                  <span className='pos-top d-flex flex-column'>
+                    <Button
+                      variant='warning'
+                      size='sm'
+                      className='align-top p-0'
+                    >
+                      <h6 className='mb-0'>&times;</h6>
+                    </Button>
+                    <Button
+                      variant='warning'
+                      size='sm'
+                      className='align-top p-0'
+                    >
+                      <small className='mb-0'>▼</small>
+                    </Button>
+                  </span>
+                </th>
+                <th className='position-relative'>
+                  <span>created date time</span>
+                  <span className='pos-top d-flex flex-column'>
+                    <Button
+                      variant='warning'
+                      size='sm'
+                      className='align-top p-0'
+                    >
+                      <h6 className='mb-0'>&times;</h6>
+                    </Button>
+                    <Button
+                      variant='warning'
+                      size='sm'
+                      className='align-top p-0'
+                    >
+                      <small className='mb-0'>▼</small>
+                    </Button>
+                  </span>
+                </th>
+                <th className='position-relative'>
+                  <span>last updated date time</span>
+                  <span className='pos-top d-flex flex-column'>
+                    <Button
+                      variant='warning'
+                      size='sm'
+                      className='align-top p-0'
+                    >
+                      <h6 className='mb-0'>&times;</h6>
+                    </Button>
+                    <Button
+                      variant='warning'
+                      size='sm'
+                      className='align-top p-0'
+                    >
+                      <small className='mb-0'>▼</small>
+                    </Button>
+                  </span>
+                </th>
+                <th>
+                  <span>actions</span>
+                </th>
               </tr>
             </thead>
             <tbody className='small'>
-              <tr>
-                <td>1</td>
-                <td className='text-green'>1234</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                <td>@mdo</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td colSpan='2'>Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
+              <TableRow />
+              <TableRow />
+              <TableRow />
+              <TableRow />
+              <TableRow />
             </tbody>
           </Table>
         </Modal.Body>
-        <Modal.Footer>
+        <Modal.Footer className='border-0'>
           <Form.Row className='w-100 align-items-center mx-0'>
             <Col sm={2} className='my-1'>
-              <Button
-                block
-                className='py-3'
-                variant='secondary'
-                onClick={onHide}
-              >
-                Close
-              </Button>
+              <Form.Group className='row mx-0 align-items-center'>
+                <Form.Control className='col-3' type='text' value='10' />
+                <Form.Label className='col'>rows per page</Form.Label>
+              </Form.Group>
+            </Col>
+
+            <Col className='my-1 mx-auto d-flex'>
+              <Pagination className='mx-auto'>
+                <Pagination.Prev />
+                <Pagination.Item active>{1}</Pagination.Item>
+                <Pagination.Item>{2}</Pagination.Item>
+                <Pagination.Item>{3}</Pagination.Item>
+                <Pagination.Item>{4}</Pagination.Item>
+                <Pagination.Ellipsis />
+                <Pagination.Next />
+              </Pagination>
             </Col>
             <Col sm={2} className='my-1'>
-              <Button block className='py-3' variant='secondary' type='reset'>
-                Reset
-              </Button>
-            </Col>
-            <Col className='my-1'>
-              <Form.Label htmlFor='inlineFormInputName' srOnly>
-                Name
-              </Form.Label>
-              <Form.Control
-                id='inlineFormInputName'
-                className='py-4'
-                placeholder='Enter A Name to Save Results'
-              />
-            </Col>
-            <Col sm={2} className='my-1'>
-              <Button block className='py-3' variant='dark' type='submit'>
-                Generate
-              </Button>
+              <Form.Group className='row mx-0 align-items-center justify-content-end'>
+                <Form.Control className='col-3' type='text' />
+                <Form.Label className='col-3'>
+                  <Button variant='link' className='p-0'>
+                    Go
+                  </Button>
+                </Form.Label>
+              </Form.Group>
             </Col>
           </Form.Row>
         </Modal.Footer>
